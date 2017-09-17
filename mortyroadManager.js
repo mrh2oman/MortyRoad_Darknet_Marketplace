8e3759c668c7b1022cd62945ae75bbf1bdfed4a6
David Waterman <davidwaterman@Davids-MacBook-Air.local>
2017-09-17 12:05:35 -0500

    Add inventory works

--- mortyroadManager.js
+++ mortyroadManager.js
@@ -80,5 +80,47 @@ function viewLowInventory() {
       setTimeout(mortyManager,10000);
   });
 }
+function addInventory() {
+  connection.query('SELECT * FROM products', function(err, items){
+    if (err) throw err;
+    for(var i = 0; i<items.length; i++){
+      console.log(items[i].item_id + "  ||  ".red+items[i].product_name+"  ||  ".red+items[i].department_name+"  ||  ".red+items[i].price+"  ||  ".red+items[i].stock_quantity+"  ||  ".red);
+    }
+    inquirer.prompt([
+      {
+        type:"input",
+        message: "Which Item would you like to increase inventory for?".underline.green,
+        name:"id"
+      },
+      {
+        type:"input",
+        message:"How many units would you like to increase inventory?".underline.green,
+        name:"quantity"
+      }
 
+    ]).then (function (po){
+      var quantity = po.quantity;
+      var id = po.id;
+      var quan = parseInt(quantity);
+      connection.query('SELECT * FROM products WHERE item_id=' + id, function(err, pickedItem){
+        if (err) throw err;
+        if(quantity > 0) {
+          connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [pickedItem[0].stock_quantity + quan, id],
+          function(err, items) {
+            if (err) throw err;
+
+            console.log("Inventory " + pickedItem[0].name + " has been increased by " + quantity);
+
+            setTimeout(mortyManager, 3000);
+          });
+
+        } else {
+          console.log("You want to add nothing? What are you, some kind of Jerry?".bgWhite.red);
+          console.log("Let's try this again, and pay attention to the quantity this time!!!!".bgBlack.magenta);
+          setTimeout(mortyManager, 3000);
+         }
+      });
+    });
+  });
+}
 // console.log("Inventory for " + items[i].item_id + "  ||  ".red + items[i].product_name+"  ||  ".red " is good man");
